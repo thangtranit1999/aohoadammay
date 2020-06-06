d36f142bcd2321d66621481fae62a3fa6d5574aa
thangtranit1999 <ngocthangkt27@gmail.com>
2020-06-06 13:09:41 +0700

    dasd

--- database.js
+++ database.js
@@ -1,10 +1,11 @@
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
 var mongodb = require('mongodb');
 
 //We need to work with "MongoClient" interface in order to connect to a mongodb server.
-var MongoClient = mongodb.MongoClient({useUnifiedTopology:true});
-console.log("co chay");
+var MongoClient = mongodb.MongoClient;
+
 // Connection URL. This is where your mongodb server is running.
-var url = 'mongodb://10.110.10.106/test';
+var url = 'mongodb://10.110.10.106:27017/test';
 
 // Use connect method to connect to the Server
 MongoClient.connect(url, function (err, db) {
@@ -12,7 +13,7 @@ MongoClient.connect(url, function (err, db) {
     console.log('Unable to connect to the mongoDB server. Error:', err);
   } else {
     //HURRAY!! We are connected. :)
-    console.log('Connection to', url);
+    console.log('Connection established to', url);
 
     // do some work here with the database.
 
