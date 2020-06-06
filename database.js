986c3fccf3c78a5a3de1ebea319b27bf7dbc0ba7
thangtranit1999 <49776697+thangtranit1999@users.noreply.github.com>
2020-06-06 14:20:19 +0700

    Update database.js
    asdas

--- database.js
+++ database.js
@@ -5,7 +5,7 @@ var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
 
 // Connection URL. This is where your mongodb server is running.
-var url = 'mongodb://10.110.10.106/test';//:27017
+var url = 'mongodb://52.77.65.178/test';//:27017
 
 // Use connect method to connect to the Server
 MongoClient.connect(url, function (err, db) {
