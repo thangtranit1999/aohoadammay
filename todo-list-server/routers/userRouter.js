b50c62aadad0b856c1c7fc0c2f7c333686c070c6
thangtranit1999 <ngocthangkt27@gmail.com>
2020-06-09 17:56:53 +0700

    asda

--- todo-list-server/routers/userRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const Router = express.Router();
-const { login, post } = require('../controllers/userController');
-
-Router.post('/login', login);
-Router.post('', post);
-
-module.exports = Router;
